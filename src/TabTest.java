c12eef386ab540bef9d633155c1426385c17bfea
yupi <grafik@10g.pl>
2017-04-17 17:11:02 +0200

    przestało działać po ddadaniu wyjątków

--- src/TabTest.java
+++ src/TabTest.java
@@ -4,28 +4,31 @@ import java.util.Scanner;
 
 public class TabTest {
     public static void main(String[] args) {
-        System.out.println("Podaj wartość X: ");
+
         Scanner scanner = new Scanner(System.in);
-
-        try {
-            int x = scanner.nextInt();
-
-            System.out.println(" podaj wartość Y: ");
-            int y = scanner.nextInt();
-            if (x < 0 && y > 0) {
-                System.out.println("ćwiartka 2");
-            } else if (x < 0 && x < y) {
-                System.out.println("ćwiartka 3");
-            } else if (x > 0 && y > 0) {
-                System.out.println("ćwiartka 1");
-            } else if (x > 0 && y < 0) {
-                System.out.println("ćwiartka 4");
-            } else if (x == 0 && y == 0) {
-                System.out.println("punkt 0.0");
+        boolean numberCorrect = true;
+        while (!numberCorrect)
+            try {
+                System.out.println("Podaj wartość X: ");
+                double x = scanner.nextInt();
+                numberCorrect = true;
+                System.out.println(" podaj wartość Y: ");
+                double y = scanner.nextDouble();
+                if (x < 0 && y > 0) {
+                    System.out.println("ćwiartka 2");
+                } else if (x < 0 && x < y) {
+                    System.out.println("ćwiartka 3");
+                } else if (x > 0 && y > 0) {
+                    System.out.println("ćwiartka 1");
+                } else if (x > 0 && y < 0) {
+                    System.out.println("ćwiartka 4");
+                } else if (x == 0 && y == 0) {
+                    System.out.println("punkt 0.0");
+                }
+            } catch (InputMismatchException ex) {
+                numberCorrect = false;
+                System.out.println("błedna wartość, wprowadz cyfrę");
             }
-        } catch (InputMismatchException ex) {
-            System.out.println("błedna wartość, wprowadz cyfrę całkowita");
-        }
     }
 
 }
